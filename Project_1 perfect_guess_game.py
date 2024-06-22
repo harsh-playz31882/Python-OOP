2fb8173270d683093ec5097eddcc2cda94f38893
Harsh Jethwa <134230102+harsh-playz31882@users.noreply.github.com>
2024-06-22 14:44:14 +0530

    Update Project_1 perfect_guess_game.py

--- Project_1 perfect_guess_game.py
+++ Project_1 perfect_guess_game.py
@@ -21,6 +21,6 @@ while(a != n):
 print(f'''you have guessed correctly in {guess} attempt
       if you guessed in less than 10 attemps well played
       if you guessed in less than 20 attempts acceptable
-      but if you guessed in more than 20 attempts, you are noob''')
+      if you guessed in more than 20 attempts, you are noob''')
 
 
